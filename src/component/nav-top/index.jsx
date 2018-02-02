23ec67d3a122c2a55975f9a1189e47a24f8c36d1
LYX <Rile14929@163.com>
2018-02-02 17:17:42 +0800

    通用部分

---
+++ src/component/nav-top/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+
+class NavSide extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    onlogout(){
+
+    }
+    render(){
+        return(
+            <div className="navbar navbar-default top-navbar">
+            <div className="navbar-header">
+                <Link className="navbar-brand" to="/"><b>HAPPY</b>MMALL</Link>
+            </div>
+
+            <ul className="nav navbar-top-links navbar-right">
+                <li className="dropdown">
+                    <a className="dropdown-toggle" href="javascript:;">
+                        <i className="fa fa-user fa-fw"></i>
+                        <span>欢迎admin xxx</span>
+                        <i className="fa fa-caret-down"></i>
+                    </a>
+                    <ul className="dropdown-menu dropdown-user">
+                        <li>
+                            <a onClick={()=>{this.onlogout()}}><i className="fa fa-sign-out fa-fw"></i> 退出登录</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        )
+    }
+}   
+export default NavSide
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,12 @@ module.exports = {
     publicPath:'/dist/',
     filename: 'js/app.js'
   },
+  resolve:{
+    alias:{
+      page:path.resolve(__dirname, 'src/page'),
+      component:path.resolve(__dirname, 'src/component')
+    }
+  },
   module:{
     rules: [
       // reac文件的处理
@@ -68,7 +74,8 @@ module.exports = {
   plugins:[
     // 处理html
     new HtmlWebpackPlugin({
-      template:'./src/index.html'
+      template:'./src/index.html',
+      favicon:'./favicon.ico'
     }),
     // 独立css文件
     new ExtractTextPlugin("css/styles.css"),
@@ -79,6 +86,9 @@ module.exports = {
     })
 ],
     devServer: {
-      port:8086
+      port:8086,
+      historyApiFallback:{
+        index:'/dist/index.html'
+      }
     },
 };
